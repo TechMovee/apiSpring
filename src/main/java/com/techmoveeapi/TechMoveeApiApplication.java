a7361726759d31e8e4944a2e9f2254bb71a59862
Beatriz Belaparte Favero <beatrizfavero-ieg@instituto.ad>
2024-08-19 07:42:19 -0300

    first commit

---
+++ src/main/java/com/techmoveeapi/TechMoveeApiApplication.java
@@ -0,0 +1,13 @@
+package com.techmoveeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMoveeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechMoveeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techmoveeapi/TechMoveeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmoveeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMoveeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
