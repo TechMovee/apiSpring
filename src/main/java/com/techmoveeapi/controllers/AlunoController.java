72ef50e26dd4bee811954885d394010285c60db2
Matheus Melo <matheus.melo@germinare.org.br>
2024-09-09 11:20:13 -0300

    Começando Controller do Aluno

--- src/main/java/com/techmoveeapi/controllers/AlunoController.java
+++ src/main/java/com/techmoveeapi/controllers/AlunoController.java
@@ -1,4 +1,13 @@
 package com.techmoveeapi.controllers;
 
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aluno")
+@Validated
 public class AlunoController {
+
 }
