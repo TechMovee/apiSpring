943dabf03be8f8dbf38191f969192d325c964d5e
TechMovee <TechMovee11@gmail.com>
2024-09-20 12:23:31 -0300

    fazendo repo de fotos

--- src/main/java/com/techmoveeapi/repository/FotosRepository.java
+++ src/main/java/com/techmoveeapi/repository/FotosRepository.java
@@ -1,4 +1,19 @@
 package com.techmoveeapi.repository;
 
+import com.techmoveeapi.model.Endereco;
+import com.techmoveeapi.model.Fotos;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
 public interface FotosRepository {
+    Optional<Fotos> findByID(int id);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Fotos e WHERE e.id = ?1")
+    void deleteById(int id);
 }
