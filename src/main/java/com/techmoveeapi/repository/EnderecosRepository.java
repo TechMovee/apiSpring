7088a2fef13ad65f9e23451796225da5ca44a3eb
TechMovee <TechMovee11@gmail.com>
2024-09-20 11:41:08 -0300

    ajustando repo de aluno e fazendo de endereco

--- src/main/java/com/techmoveeapi/repository/EnderecosRepository.java
+++ src/main/java/com/techmoveeapi/repository/EnderecosRepository.java
@@ -1,4 +1,19 @@
 package com.techmoveeapi.repository;
 
+import com.techmoveeapi.model.Endereco;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface EnderecosRepository {
+    Optional<Endereco> findByID(int id);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Endereco e WHERE e.id = ?1")
+    void deleteById(int id);
 }
