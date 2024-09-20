d9c3b298759e6ece8076ac95fe415184bf0e2f94
TechMovee <TechMovee11@gmail.com>
2024-09-20 17:24:17 -0300

    criação do repo de endereco

--- src/main/java/com/techmoveeapi/repository/EnderecosRepository.java
+++ src/main/java/com/techmoveeapi/repository/EnderecosRepository.java
@@ -2,6 +2,7 @@ package com.techmoveeapi.repository;
 
 import com.techmoveeapi.model.Endereco;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface EnderecosRepository {
+public interface EnderecosRepository extends JpaRepository<Endereco, Integer> {
     Optional<Endereco> findByID(int id);
 
     @Modifying
